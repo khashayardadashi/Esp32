ae03f0bf048d24f6b288776e32ab2ed2d84b9335
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2025-11-24 13:20:52 +0330

    Add files via upload

---
+++ Bluetooth/Led _ VoiceController.ino
@@ -0,0 +1,25 @@
+#include "BluetoothSerial.h"
+BluetoothSerial BT;
+String w;
+void setup(){
+  BT.begin("Esp32");
+  Serial.begin(115200);
+  pinMode(23, OUTPUT);
+  pinMode(15, INPUT);
+}
+void loop(){
+  if(BT.available()){
+    w=BT.readString();
+    if(w == "روشن شو"){
+      digitalWrite(23,1);
+      BT.print ("led turn on");
+    }
+    else if (w == "خاموش شو"){
+      digitalWrite(23, 0);
+      BT.print("led turn off");
+    }
+    else{
+      digitalWrite(23,0);
+    }
+  }
+}
